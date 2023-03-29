b7129730149c31ad73ea800d61d06a91e8ab5db6
sidande2022 <117272901+sidande2022@users.noreply.github.com>
2023-03-29 14:09:49 +0800

    Create  Chatgpt.vue

---
+++ src/Chatgpt.vue
@@ -0,0 +1,81 @@
+<template>
+  <div class="chatgpt-container">
+    <h1 class="chatgpt-title">Chatgpt 知识库</h1>
+    <p class="chatgpt-description">
+      欢迎来到 Chatgpt 知识库，这里收集了各种有用的知识和技巧，帮助您更好地使用 Chatgpt。
+    </p>
+    <div class="chatgpt-content">
+      <h2 class="chatgpt-section-title">如何使用 Chatgpt？</h2>
+      <ul class="chatgpt-list">
+        <li class="chatgpt-list-item">了解 Chatgpt 的基本概念和功能</li>
+        <li class="chatgpt-list-item">掌握如何与 Chatgpt 进行交互</li>
+        <li class="chatgpt-list-item">学会如何使用 Chatgpt 提供的各种工具和插件</li>
+      </ul>
+      <h2 class="chatgpt-section-title">Chatgpt 相关资源</h2>
+      <ul class="chatgpt-list">
+        <li class="chatgpt-list-item"><a href="#">Chatgpt 官网</a></li>
+        <li class="chatgpt-list-item"><a href="#">Chatgpt GitHub 仓库</a></li>
+        <li class="chatgpt-list-item"><a href="#">Chatgpt 文档</a></li>
+      </ul>
+    </div>
+    <div class="chatgpt-footer">
+      <p>版权所有 &copy; Chatgpt</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Chatgpt',
+};
+</script>
+
+<style scoped>
+.chatgpt-container {
+  max-width: 800px;
+  margin: 0 auto;
+  padding: 20px;
+}
+
+.chatgpt-title {
+  font-size: 36px;
+  font-weight: bold;
+  text-align: center;
+}
+
+.chatgpt-description {
+  margin-top: 20px;
+  font-size: 18px;
+  line-height: 1.5;
+  text-align: center;
+}
+
+.chatgpt-content {
+  margin-top: 40px;
+}
+
+.chatgpt-section-title {
+  font-size: 24px;
+  font-weight: bold;
+  margin-top: 30px;
+}
+
+.chatgpt-list {
+  margin-top: 20px;
+  list-style-type: disc;
+  margin-left: 20px;
+}
+
+.chatgpt-list-item {
+  margin-top: 10px;
+}
+
+.chatgpt-list-item a {
+  color: #007bff;
+}
+
+.chatgpt-footer {
+  margin-top: 40px;
+  text-align: center;
+}
+</style>
