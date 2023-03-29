2151cd270012b0f98dd99fabeaf18fb5c05f82e2
sidande2022 <117272901+sidande2022@users.noreply.github.com>
2023-03-29 14:10:08 +0800

    Create app.vue

---
+++ src/app.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <Chatgpt />
+  </div>
+</template>
+
+<script>
+import Chatgpt from './Chatgpt.vue';
+
+export default {
+  name: 'App',
+  components: {
+    Chatgpt,
+  },
+};
+</script>
